8b5d76d98aa41f4f9c345cd5fab0e552d2bcdbe7
Haris Beg (r625361) <haris.beg@cambiahealth.com>
2016-01-26 09:56:39 -0800

    Deploy Web app as a WAR (not a Jar)

--- src/main/java/demo/UdemyMicroServicesBootMavenApplication.java
+++ src/main/java/demo/UdemyMicroServicesBootMavenApplication.java
@@ -2,11 +2,27 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UdemyMicroServicesBootMavenApplication {
+public class UdemyMicroServicesBootMavenApplication extends SpringBootServletInitializer {
 
+	/**
+	 * Used when running as a JAR
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(UdemyMicroServicesBootMavenApplication.class, args);
+	}
+
+	/**
+	 * Used when running as a WAR
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(
+			SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(UdemyMicroServicesBootMavenApplication.class);
 	}
 }
