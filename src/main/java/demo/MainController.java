027c8274c56c29ce787a1324dbc18113be1d2122
Haris Beg (r625361) <haris.beg@cambiahealth.com>
2016-01-26 14:29:21 -0800

    REST Controllers in Spring MVC returning JSON response
    This one returns a domain object called Team when we request the
    http://localhost:8088/demo/hi, as we see it as a JSON object in the
    browser.

--- src/main/java/demo/MainController.java
+++ src/main/java/demo/MainController.java
@@ -1,18 +1,33 @@
 package demo;
 
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import demo.domain.Player;
+import demo.domain.Team;
+
+@RestController
 public class MainController {
 	
-	@RequestMapping("/hi/{name}")
-	public  String hiThere(Map model, @PathVariable String name) {
-		model.put("name", name);
-		return "hello";
+	private Team team;
+	
+	@PostConstruct
+	public void init() {
+		Set<Player> players = new HashSet<>();
+		players.add(new Player("Charlie Brown", "pitcher"));
+		players.add(new Player("Snoopy", "shortstop"));
+		
+		team = new Team("California", "Peanuts", players);
+	}
+	
+	@RequestMapping("/hi")
+	public Team hiThere() {
+		return team;
 	}
 
 }
